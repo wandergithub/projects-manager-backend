7ec87c70c76f25d81c5ffa3a9484ea350b7a06f2
wandergithub <wandergonzalez0@hotmail.com>
2022-11-15 11:35:42 -0400

    add changes so routes work with project model

--- routes/routes.js
+++ routes/routes.js
@@ -1,73 +1,70 @@
-const express = require('express');
-const Model = require('../models/projectModel');
+const express = require("express");
+const Model = require("../models/projectModel");
 
-const router = express.Router()
+const router = express.Router();
 
 //Post Method
-router.post('/post', async (req, res) => {
+router.post("/post", async (req, res) => {
   const data = new Model({
     name: req.body.name,
-    age: req.body.age
-  })
+    description: req.body.description,
+    stack: req.body.stack,
+    links: req.body.links,
+    hidden: req.body.hidden,
+    app_photo: req.body.app_photo,
+  });
 
   try {
     const dataToSave = await data.save();
     res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
-  catch (error) {
-    res.status(400).json({message: error.message});
-  }
-})
+});
 
 //Get all Method
-router.get('/getAll', async (req, res) => {
-  try{
-      const data = await Model.find();
-      res.json(data)
+router.get("/getAll", async (req, res) => {
+  try {
+    const data = await Model.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  catch(error){
-      res.status(500).json({message: error.message})
-  }
-})
+});
 //Get by ID Method
-router.get('/getOne/:id', async (req, res) => {
-  try{
+router.get("/getOne/:id", async (req, res) => {
+  try {
     const data = await Model.findById(req.params.id);
-    res.json(data)
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  catch(error){
-      res.status(500).json({message: error.message})
-  }
-})
+});
 
 //Update by ID Method
-router.patch('/update/:id', async (req, res) => {
+router.patch("/update/:id", async (req, res) => {
   try {
-      const id = req.params.id;
-      const updatedData = req.body;
-      const options = { new: true };
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
 
-      const result = await Model.findByIdAndUpdate(
-          id, updatedData, options
-      )
+    const result = await Model.findByIdAndUpdate(id, updatedData, options);
 
-      res.send(result)
+    res.send(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
-  catch (error) {
-      res.status(400).json({ message: error.message })
-  }
-})
+});
 
 //Delete by ID Method
-router.delete('/delete/:id', async (req, res) => {
+router.delete("/delete/:id", async (req, res) => {
   try {
-      const id = req.params.id;
-      const data = await Model.findByIdAndDelete(id)
-      res.send(`Document with ${data.name} has been deleted..`)
+    const id = req.params.id;
+    const data = await Model.findByIdAndDelete(id);
+    res.send(`Document with ${data.name} has been deleted..`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
-  catch (error) {
-      res.status(400).json({ message: error.message })
-  }
-})
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,36 +1,35 @@
 // Global ENV variables
-require('dotenv').config();
+require("dotenv").config();
 const URL = process.env.DATABASE_URL;
 
 // Import dependencies modules.
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const port = 3000;
 
 // Import project files
-const routes = require('./routes/routes');
-
+const routes = require("./routes/routes");
 
 // db connection
 mongoose.connect(URL);
 const db = mongoose.connection;
 
-db.on('error', (error) => {
+db.on("error", (error) => {
   console.log(error);
-})
+});
 
-db.once('connected', () => {
-  console.log('Database Connected');
-})
+db.once("connected", () => {
+  console.log("Database Connected");
+});
 // -----------------------------------
 // Server behavior
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', routes);
+app.use("/api", routes);
 
 app.listen(port, () => {
-    console.log(`Server Started at ${port}`)
-})
+  console.log(`Server Started at ${port}`);
+});
 // -----------------------------------
